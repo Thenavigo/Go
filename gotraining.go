5fe442b1cd0bb49d92d6f848d83fbf08bd533c2e
patbi <biyagapatrick@gmail.com>
2022-12-18 16:03:49 +0100

    first commit

---
+++ gotraining.go
@@ -0,0 +1,38 @@
+package main
+
+// Import multiple packages
+// You could use an alias like f "fmt"
+
+import(
+	"bufio"
+	f "fmt"	
+	"log"
+	"os"
+)
+
+
+// Create alias to long function names
+
+var pl = f.Println
+
+func main(){
+	// Prints text and a newline
+	// List package name followed by a period and the function name
+	pl("Hello Go")
+
+	// Get user input (To run this in the terminal go run hellogo.go)
+	pl("Where do you name ?")
+	// Setup buffered reader that gets text from the keyboard
+	reader := bufio.NewReader(os.Stdin)
+	// Copy text up to the newline
+	// The blank identifier _ will get err and ignore it (Bad Practice)
+	// name, _ := reader.ReadString('\n')
+	// It is better to handle it
+	name, err := reader.ReadString('\n')
+	if err == nil {
+		pl("Hello", name)
+	} else {
+		// Log this error
+		log.Fatal(err)
+	}
+}
