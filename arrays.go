3e34d892aaf741159615adb39928f2d9acbc7b0b
patbi <biyagapatrick@gmail.com>
2022-12-28 22:25:19 +0100

    arrays.go

---
+++ arrays.go
@@ -0,0 +1,70 @@
+package main
+
+import(
+	f "fmt"
+)
+
+var pl = f.Println
+
+func main() {
+	// ----- ARRAYS -----
+	// Collection of values with the same data type
+	// and the size can't be changed
+	// Default values are 0, 0.0, false or ""
+
+	// Declare integer array with 5 elements
+
+	var arr1 [5]int
+
+	// Assign value to index
+	arr1[0] = 1
+	pl("Index :", arr1)
+	pl("Index 0 :", arr1[0])
+
+	// Declare and initialize
+	arr2 := [5]int{1,2,3,4,5}
+
+	// Get by index
+	pl("Index 0 :", arr2[0])
+
+	// Length
+	pl("Array Length :", len(arr2))
+
+	// Iterate with index
+	for i := 0; i < len(arr2); i++ {
+		pl(arr2[i])
+	}
+
+	// Iterate with range
+	for i, v := range arr2 {
+		f.Printf("%d : %d\n", i, v)
+	}
+
+
+	// Multidimensional Array
+	arr3 := [2][2]int {
+		{1, 2},
+		{3, 4},
+	}
+
+
+	// Print multidimensional array
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			pl(arr3[i][j])
+		}
+	}
+
+
+	// String into slice of runes
+	aStr1 := "abcde"
+	rArr := []rune(aStr1)
+	for _, v := range rArr {
+		f.Printf("Rune Array : %d\n", v)
+	}
+
+	// Byte array to string
+	byteArr := []byte{'a', 'b', 'c'}
+	bStr := string(byteArr[:])
+	pl("I'm a string :", bStr)
+}
